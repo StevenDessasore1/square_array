b9e1ba4a671f64b45e4a5e0014b0ff814e756b2e
StevenDessasore <stevendessasoreba@gmail.com>
2021-03-09 04:23:36 -0500

    commit tree 6b0859034834fd5ac969b2f18efc2de1f3b87bf6

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+#def square_array(array)
+  #new_array = []
+  #array.each do |array|
+    #new_array << (array ** 2)
+  #end
+
+  ##return new_array
+#end
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |array|
+    new_array.push(array ** 2)
+  end
+  return new_array
 end
